3a916596a1f5f64d5c115ddeee976de15774e28e
tgoodric <tgoodric@spsu.edu>
2015-04-11 21:06:00 -0400

    Most recent  version. File I/O currently borked.

--- BookstorePage/BookstorePage/Book.cs
+++ BookstorePage/BookstorePage/Book.cs
@@ -11,7 +11,7 @@ namespace BookstorePage
         public string Author { get; set; }
         public string ISBNNumber { get; set; }
         public string Professor { get; set; }
-        public string Edition { get; set; }
+        public string Use { get; set; }
         public int QtyNew { get; set; }
         public int QtyUsed { get; set; }
         public int QtyRental { get; set; }
@@ -26,15 +26,15 @@ namespace BookstorePage
         public string Course { get; set; }
         public string Section { get; set; }
 
-        public Book(string title, string author, string isbn, string prof, string ed, int qtyN, int qtyU, int qtyR,
-                    bool eBookAvail, string descr, float priceN, float priceU, float priceR, float priceE,
-                    string crn, string semest, string cour, string sect) //massive constructor for a not-terribly-complex object
+        public Book(string isbn, string title, string author, string semest, string cour, string sect, string prof, string crn, string use, int qtyN, int qtyU, int qtyR,
+                    bool eBookAvail, float priceN, float priceU, float priceR, float priceE,
+                    string descr) //massive constructor for a not-terribly-complex object
         {
             Title = title;
             Author = author;
             ISBNNumber = isbn;
             Professor = prof;
-            Edition = ed;
+            Use = use;
             QtyNew = qtyN;
             QtyUsed = qtyU;
             QtyRental = qtyR;
@@ -56,7 +56,7 @@ namespace BookstorePage
             this.Course = other.Course;
             this.ISBNNumber = other.ISBNNumber;
             this.Professor = other.Professor;
-            this.Edition = other.Edition;
+            this.Use = other.Use;
             this.QtyNew = other.QtyNew;
             this.QtyUsed = other.QtyUsed;
             this.QtyRental = other.QtyRental;
@@ -76,6 +76,28 @@ namespace BookstorePage
          * Getters and setters have been implemented as properties.
          * Changes to object state come from webpage
          */
+        public Book(string[] components)    //workaround for a nasty constructor call
+        {                                   //components is a string[] with the components in the above
+            this.ISBNNumber=components[0];  //constructor
+            this.Title=components[1];
+            this.Author=components[2];
+            this.Semester=components[3];
+            this.Course=components[4];
+            this.Section=components[5];
+            this.Professor=components[6];
+            this.CRNNumber=components[7];
+            this.Use=components[8];
+            this.QtyNew = Convert.ToInt32(components[9]);
+            this.QtyUsed = Convert.ToInt32(components[10]);
+            this.QtyRental = Convert.ToInt32(components[11]);
+            this.EBookAvailable = (Convert.ToInt32(components[12]) != 0); //don't ask
+            this.PriceNew = Convert.ToSingle(components[13]);
+            this.PriceUsed = Convert.ToSingle(components[14]);
+            this.PriceRental = Convert.ToSingle(components[15]);
+            this.PriceEBook = Convert.ToSingle(components[16]);
+            this.Description = components[17];
+        }
+        
         public override bool Equals(Object other)
         {
             if (other is Book)
---
+++ BookstorePage/BookstorePage/BookSearch.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookSearch.aspx.cs" Inherits="BookstorePage.BookSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Placeholder Text<br />
+        <asp:Label ID="Label1" runat="server" Text="Results"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
